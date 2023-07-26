7b73f9bcbd5fe9fc1ed0c97c66ead7a9efe2055a
root <root@Wagner-PC>
2023-07-26 19:00:45 -0300

    updating

--- node/index.js
+++ node/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config ={
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Wagner')`
+
+connection.query(sql)
+connection.end()
 
 app.get('/', (req,res) => {
     res.send('<h1>Html Page Test</h1>')
